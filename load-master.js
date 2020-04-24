10ae462d37a3ec90d4b318926f5756999cd96f35
ippolito <marco.ippolito@microcenter.it>
2020-04-25 01:09:25 +0200

    tested heavily

--- load-master.js
+++ load-master.js
@@ -1,35 +1,35 @@
-const fs = require('fs');
-const spawner = require('child_process');
-const _ = require('lodash');
-const source = "./tickers-list";
-
-const apiKey = process.argv[2];
-const instances = process.argv[3];
-
+const fs = require('fs'),
+    spawner = require('child_process'),
+    _ = require('lodash'),
+    apiKey = process.argv[2],
+    instances = process.argv[3],
+    source = process.argv[4],
+    result = [];
 
 loadTickerDetails();
 
-
 function loadTickerDetails() {
-    const tickersNameArray = fs.readFileSync(source).toString('utf-8').split("\n");
-    const chunk = _.round((_.divide(tickersNameArray.length, instances)));
-    const result = [];
+    console.time('Process exited in');
     let counter = 0;
-    _.chunk(tickersNameArray, chunk).forEach(arrayChunk => {
-        spawner.fork(`${__dirname}/load-worker.js`, [apiKey]).on('message', (response) => {
-            result.push(...response);
-        }).on('exit', () => {
-            console.log(`COUNTER: ${counter}`)
-            counter++;
-        }).send(arrayChunk);
-    });
+    const tickersNameArray = fs.readFileSync(source).toString('utf-8').split("\n"),
+        chunk = _.round(_.divide(tickersNameArray.length, instances)),
+        chucksCreated = _.chunk(tickersNameArray, chunk);
 
-    while (counter !== 5) {
-        console.log(result);
-        fs.writeFile(`export-${new Date().getTime()}`, JSON.stringify(result), (error) => {
-            if (error) {
-                console.error(`Error: ${error}`);
-            }
+    chucksCreated.forEach(arrayChunk => {
+        spawner.fork(`${__dirname}/load-worker.js`, [apiKey])
+            .on('message', (response) => {
+                counter++;
+                result.push(response);
+                writeFile(counter, _.size(chucksCreated));
+            }).send(arrayChunk);
+    });
+}
+
+function writeFile(counter, chuncksNumber) {
+    if (counter === chuncksNumber) {
+        fs.writeFileSync(`./export-${new Date().getTime()}`, JSON.stringify(_.flatten(result)), (error) => {
+            console.assert(error, `Error: ${error}`);
         });
+        console.timeEnd('Process exited in');
     }
 }
